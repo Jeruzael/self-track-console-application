fae14ffdd55e4881669e82e6d48c3790c470eae2
Jeruzael <dumale.jeruzael.bscs2019@gmail.com>
2023-11-22 14:05:01 +0800

    Usertime

--- data/handler.py
+++ data/handler.py
@@ -1,35 +1,75 @@
-from datetime import date
 import datetime
+from datetime import timedelta as td
+from datetime import date
+from datetime import datetime as dt
 import time
 
-class Center:
-    year: str = ""
-    month: str = ""
-    week: str = ""
-    day: str = ""
+class Timecapsule:
+    __leapYearStart: int = 2020
+    __year: int = 0
+    __month: str = ""
+    __week: int = 0
+    __day: int = 0
     date: date
-    time: str = ""
+    time: time
 
     def __init__(self) -> None:
-        self.year = self.getYear()
-        self.month = self.getMonth()
-        self.week = self.getWeek()
-        self.day = self.getDay()
+        self.__year = self.getYear()
+        self.__month = self.getMonth()
+        self.__week = self.getWeek()
+        self.__day = self.getDay()
         self.date = date.today()
-        self.time = self.time
+        self.time = time.time()
+        self.__getYear()
+        self.__getMonth()
+        self.__getWeek()
+        self.__getDay()
+        self.__setYear()
+        self.__setMonth()
+        self.__setDay()
 
-    def getYear(self)->str:
-        return '2023'
+    def setYear(self, year: int = 2023):
+        self.__year = year
+
+    def setMonth(self, month: str = "January"):
+        self.__month  = month
+
+    def setDay(self, day: int = 1):
+        self.__day = day    
+
+    def getYear(self)->int:
+        return self.__year
     
     def getMonth(self)-> str:
-        return 'November'
+        return self.__month
     
     def getWeek(self)-> str:
-        return 'week1'
+        return f'week{self.__week}'
     
-    def getDay(self)-> str:
-        return '21'
+    def getDay(self)-> int:
+        return self.__day
     
-    def getDate(self):
+    def getDate(self):        
         print(f"Date: {self.date}")
-        print(f"Test: {datetime.datetime.utcnow()}")
+        print(f"Test: {dt.fromtimestamp(self.time)} GetTime: ")
+
+    def getTimeInterval(self, start: td, end: td):
+        return end - start
+    
+    __setYear = setYear
+    __setMonth = setMonth
+    __setDay = setDay
+    __getYear = getYear
+    __getMonth = getMonth
+    __getWeek = getWeek
+    __getDay = getDay
+    
+class Usertime(Timecapsule):
+
+    def manualTimeSet(self, year, month, day):
+        self.setYear(year)
+        self.setMonth(month)
+        self.setDay(day)
+
+    def getDateInfo(self):
+        print(f"Year: {self.getYear()}\nMonth: {self.getMonth()}\nDay: {self.getDay()}")
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from model.scene import Dashboard
 from data.data import Sample
-from data.handler import Center
+from data.handler import Timecapsule as tc
+from data.handler import Usertime
 
 data = Sample()
 #sc = Dashboard("09")
 #sc.scene(data.Act)
 #sc.test(data.Act)
 
-c = Center()
-c.getDate()
+
